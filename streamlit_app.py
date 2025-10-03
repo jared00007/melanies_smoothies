27fee76b4a1e6f92221ed2f6df7ba6bd961e3732
jared00007 <jaredsmcgrath@yahoo.com>
2025-10-02 21:26:16 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,9 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title('My Parents New Healthy Diner')
-st.header("Breakfast Menu")
-st.write("""
-🥣 Omega 3 & Blueberry Oatmeal  
-🥤 Kale, Spinach & Rocket Smoothie  
-🍳 Hard-Boiled Free-Range Egg
-""")
+st.title(f":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
+st.write("""Choose the fruits you want in your custom Smoothie
+    """)
 
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your Smoothie will be:', name_on_order)
